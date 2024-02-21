c149094bd5bf9bf827851ad94ff74a25b97c85aa
Evan Harrigan <harriganevan@gmail.com>
2024-02-21 16:47:52 -0500

    add basic search functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,9 @@ const socket = io.connect('http://localhost:3000');
 function App() {
 
   const [links, setLinks] = useState([]);
+  const [search, setSearch] = useState('');
+  const [timerId, setTimerId] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   const getLinks = async () => {
     const response = await fetch('http://localhost:3000/links');
@@ -31,14 +34,28 @@ function App() {
     const response = await fetch(`http://localhost:3000/links/${link.replaceAll('/', '%2F')}`, {
       method: 'POST',
     });
-    const newLinks = await response.json();
-    console.log(newLinks)
+  }
+
+  const delayedFunction = async (value) => {
+    console.log('delayed search', value);
+    const response = await fetch(`https://en.wikipedia.org/w/rest.php/v1/search/title?q=${value}&limit=5`);
+    const searchResults = await response.json();
+    console.log(searchResults);
+  }
+
+  const handleChange = (e) => {
+    clearTimeout(timerId);
+    setSearch(e.target.value);
+    console.log(search);
+    setTimerId(setTimeout(() => delayedFunction(e.target.value), 500));
   }
 
   return (
     <>
       <h1>WikiBattles</h1>
-      <div>
+      <input onChange={handleChange} value={search}/>
+      <div></div>
+      <div className='buttons'>
         {links.map(link =>
           <button onClick={() => getNewLinks(link[0])}>{link[1]}</button>
         )}
