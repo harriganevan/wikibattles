c76ff0f09777da27eb757730988e3681a40fa4db
Evan Harrigan <harriganevan@gmail.com>
2024-03-21 13:41:57 -0400

    add final daily wiki page style

--- client/src/pages/Daily.jsx
+++ client/src/pages/Daily.jsx
@@ -3,15 +3,19 @@ import socket from "../socket"
 import { useEffect } from "react"
 import { useState } from "react"
 import '../wiki.css';
-// import '../testwiki.css';
 
 function Daily() {
+
 
     const [pageTitle, setPageTitle] = useState('United Kingdom');
     const [pageContent, setPageContent] = useState('');
     const [count, setCount] = useState(0);
 
+    const [startPage, setStartPage] = useState('United Kingdom');
+    const [endPage, setEndPage] = useState('Paris');
     const [route, setRoute] = useState([]);
+
+    const [gameOver, setGameOver] = useState(false);
 
     const getPage = async (page) => {
         const response = await fetch(`https://en.wikipedia.org/w/api.php?action=parse&prop=text&page=${page}&format=json&disableeditsection=1&redirects=true&useskin=minerva&origin=*`);
@@ -36,15 +40,21 @@ function Daily() {
         links.forEach((link) => {
             link.addEventListener('click', (e) => {
 
-                if(e.target.href && !e.target.href.startsWith('#')){
+                if (e.target.href && !e.target.href.startsWith('#')) {
                     e.preventDefault();
                 }
-                
+
                 if (e.target.title) {
-                    console.log(e.target.title);
-                    setPageTitle(e.target.title);
                     setCount(count + 1);
+                    setRoute([...route, e.target.title]);
+                    if (e.target.title == endPage) {
+                        setGameOver(true);
+                    } else {
+                        setPageTitle(e.target.title);
+
+                    }
                 }
+
             });
         });
 
@@ -56,15 +66,25 @@ function Daily() {
                 <h1>WikiBattles</h1>
             </Link>
             <h1>Daily puzzle</h1>
-            <h1>{count} clicks</h1>
+            <h2>{startPage} -&gt; {endPage}</h2>
+            <h2>{count} clicks</h2>
+            {!gameOver ? 
             <div className="wiki-wrapper">
                 <div className="pre-content heading-holder">
-                    <div className="page-heading"><h1>{pageTitle}</h1></div>
+                    <div className="page-heading"><h1>{pageContent != '' && pageTitle}</h1></div>
                 </div>
                 <div className="content">
                     <div dangerouslySetInnerHTML={{ __html: pageContent }} />
                 </div>
             </div>
+            :
+            <>
+                <p>you win</p>
+                {route.map((route) => 
+                    <p>{route}</p>
+                )}
+            </>}
+
         </div>
     )
 }
