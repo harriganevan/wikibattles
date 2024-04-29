c18c22f0bb76f358ace8b6a4c70a1e5368d7a9e9
Evan Harrigan <harriganevan@gmail.com>
2024-04-29 02:29:49 -0400

    fishing for better pages

--- server/src/index.js
+++ server/src/index.js
@@ -38,7 +38,7 @@ fs.readFile(process.env.DAILY_FILE, 'utf8', (err, data) => {
 });
 
 const job2 = new CronJob(
-    '0 0 0 * * *', // tick every day at midnight
+    '0 * * * * *', // tick every day at midnight
     async function () {
         const newShuffle = shuffle(randomPages);
         dailyPages.startPage = newShuffle[0];
